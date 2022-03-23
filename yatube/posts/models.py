57d0c4c344f82eb5d46f1d5af6456fad99bf85b1
artemkms <69520287+artemkms@users.noreply.github.com>
2022-03-23 23:25:24 +0700

    4

---
+++ yatube/posts/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True, null=True
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        # выводим текст поста 
+        return self.text
